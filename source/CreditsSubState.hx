c403d995f280b8003f4c56e3d35ad04fa1eb5a71
flashintv <66077024+flashintv@users.noreply.github.com>
2021-07-10 22:42:17 +0200

    poggers demo

---
+++ source/CreditsSubState.hx
@@ -0,0 +1,158 @@
+package;
+
+import flixel.FlxG;
+import flixel.FlxSprite;
+import flixel.group.FlxGroup.FlxTypedGroup;
+import flixel.text.FlxText;
+import flixel.util.FlxColor;
+
+using StringTools;
+using CoolUtil;
+
+class CreditsSubState extends MusicBeatSubstate
+{
+	var textMenuItems:Array<ListUserdata> = [];
+	var curCharacter:String = '';
+
+	var blackbar:FlxSprite;
+	var curSelected:Int = 0;
+	var usersWork:FlxText;
+	var usersText:FlxText;
+	var usersAvatar:FlxSprite;
+
+	var grpOptionsTexts:FlxTypedGroup<FlxText>;
+
+	public function new()
+	{
+		super();
+
+		var userList = CoolUtil.coolTextFile(Paths.txt('creditsMenu'));
+	
+		for (i in 0...userList.length)
+		{
+			var data:Array<String> = userList[i].split(':');
+			textMenuItems.push(new ListUserdata(data[0], data[1], Std.parseInt(data[2]), data[3], data[4]));
+		}
+
+		var bg:FlxSprite = new FlxSprite(-80).loadGraphic(Paths.image('menuBGMagenta'));
+		bg.scrollFactor.x = 0;
+		bg.scrollFactor.y = 0.15;
+		bg.setGraphicSize(Std.int(bg.width * 1.1));
+		bg.updateHitbox();
+		bg.screenCenter();
+		bg.antialiasing = true;
+		add(bg);
+
+		blackbar = new FlxSprite(420, -100).makeGraphic(10, 1000, FlxColor.BLACK);
+		add(blackbar);
+
+		grpOptionsTexts = new FlxTypedGroup<FlxText>();
+		add(grpOptionsTexts);
+
+		creditsItemsSort();
+
+		for (i in 0...textMenuItems.length)
+		{
+			var optionText:FlxText = new FlxText(20, 50 + (i * 50), 0, textMenuItems[i].curUser, 64, true);
+			optionText.setFormat(Paths.font("funkin.otf"), 64, FlxColor.WHITE, LEFT);
+			optionText.ID = i;
+			grpOptionsTexts.add(optionText);
+		}
+	}
+
+	public function addUserToList(curUser:String, curPicture:String, curNumber:Int, curWork:String, curText:String)
+	{
+		textMenuItems.push(new ListUserdata(curUser, curPicture, curNumber, curWork, curText));
+	}
+
+	function creditsItemsSort()
+	{
+		var avatarPath:String = textMenuItems[curSelected].curPicture; 
+
+		remove(usersAvatar);
+		usersAvatar = new FlxSprite(730, 60).loadGraphic(Paths.image('credits/$avatarPath'), false, 25, 25);
+		add(usersAvatar);
+
+		remove(usersWork);
+		usersWork = new FlxText(450, 300, 0, textMenuItems[curSelected].curWork, 32, true);
+		usersWork.text = StringTools.replace(usersWork.text, "\\n", " \n\n");
+		usersWork.font = 'VCR OSD Mono';
+		add(usersWork);
+
+		remove(usersText);
+		usersText = new FlxText(450, 580, 0, textMenuItems[curSelected].curText, 32, true);
+		usersText.text = StringTools.replace(usersText.text, "\\n", "\n");
+		usersText.font = 'VCR OSD Mono';
+		add(usersText);
+	}
+	
+	override function update(elapsed:Float)
+	{
+		super.update(elapsed);		
+
+		if (controls.UP_P)
+		{
+			changeSelection(-1);
+			creditsItemsSort();
+		}
+		
+
+		if (controls.DOWN_P)
+		{
+			changeSelection(1);
+			creditsItemsSort();
+		}
+
+		if (controls.BACK)
+		{
+			FlxG.sound.play(Paths.sound('cancelMenu'), 100);
+			FlxG.switchState(new MainMenuState());
+		}
+
+		if (controls.ACCEPT)
+		{	
+			if (textMenuItems[curSelected].curNumber == 1)
+			{
+				FlxG.sound.play(Paths.sound('click', 'shared'), 1);
+			}
+		}
+
+		grpOptionsTexts.forEach(function(txt:FlxText)
+		{
+			txt.color = FlxColor.WHITE;
+
+			if (txt.ID == curSelected)
+				txt.color = FlxColor.YELLOW;
+		});
+	}	
+
+	function changeSelection(change:Int = 0)
+	{
+		FlxG.sound.play(Paths.sound('scrollMenu'), 0.4);
+
+		curSelected += change;
+
+		if (curSelected < 0)
+			curSelected = textMenuItems.length - 1;
+		if (curSelected >= textMenuItems.length)
+			curSelected = 0;
+	}
+}
+
+class ListUserdata
+{
+	public var curUser:String = "";
+	public var curPicture:String = "";
+	public var curNumber:Int = 0;
+	public var curWork:String = "";
+	public var curText:String = "";
+
+	public function new(curUser:String, curPicture:String, curNumber:Int, curWork:String, curText:String)
+	{
+		this.curUser = curUser;
+		this.curPicture = curPicture;
+		this.curNumber = curNumber;
+		this.curWork = curWork;
+		this.curText = curText;
+	}
+}
---
+++ source/DialogueBox.hx
@@ -0,0 +1,283 @@
+package;
+
+import flixel.FlxG;
+import flixel.FlxSprite;
+import flixel.addons.text.FlxTypeText;
+import flixel.graphics.frames.FlxAtlasFrames;
+import flixel.group.FlxSpriteGroup;
+import flixel.input.FlxKeyManager;
+import flixel.text.FlxText;
+import flixel.util.FlxColor;
+import flixel.util.FlxTimer;
+
+using StringTools;
+
+class DialogueBox extends FlxSpriteGroup
+{
+	var box:FlxSprite;
+
+	var curCharacter:String = '';
+
+	var dialogue:Alphabet;
+	var dialogueList:Array<String> = [];
+
+	// SECOND DIALOGUE FOR THE PIXEL SHIT INSTEAD???
+	var swagDialogue:FlxTypeText;
+
+	var dropText:FlxText;
+
+	public var finishThing:Void->Void;
+
+	var portraitElectric:FlxSprite;
+	var portraitConfused:FlxSprite;
+	var portraitMad:FlxSprite;
+	var portraitAngry:FlxSprite;
+	var portraitBF:FlxSprite;
+
+	var handSelect:FlxSprite;
+	var bgFade:FlxSprite;
+
+	public function new(talkingRight:Bool = true, ?dialogueList:Array<String>)
+	{
+		super();
+
+		bgFade = new FlxSprite(-200, -200).makeGraphic(Std.int(FlxG.width * 1.3), Std.int(FlxG.height * 1.3), 0xFFB3DFd8);
+		bgFade.scrollFactor.set();
+		bgFade.alpha = 0;
+		add(bgFade);
+
+		new FlxTimer().start(0.83, function(tmr:FlxTimer)
+		{
+			bgFade.alpha += (1 / 5) * 0.7;
+			if (bgFade.alpha > 0.7)
+				bgFade.alpha = 0.7;
+		}, 5);
+
+		box = new FlxSprite(-20, 45);
+		
+		var hasDialog = false;
+		switch (PlayState.SONG.song.toLowerCase())
+		{
+			case 'pnc' | 'volt':
+				hasDialog = true;
+				box.frames = Paths.getSparrowAtlas('speech_bubble_talking', 'shared');
+				box.animation.addByPrefix('normalOpen', 'Speech Bubble Normal Open', 24, false);
+				box.animation.addByPrefix('normal', 'speech bubble normal', 24, true);
+				box.width = 200;
+				box.height = 200;
+				box.x = -100;
+				box.y = 375;
+		}
+
+		this.dialogueList = dialogueList;
+		
+		if (!hasDialog)
+			return;
+		
+		portraitElectric = new FlxSprite(-20, 80).loadGraphic(Paths.image('portraits/electricElectric', 'shared'));
+		// portraitElectric.setGraphicSize(Std.int(portraitElectric.width * PlayState.daPixelZoom * 0.9));
+		portraitElectric.updateHitbox();
+		portraitElectric.scrollFactor.set();
+		add(portraitElectric);
+		portraitElectric.visible = false;
+
+		portraitConfused = new FlxSprite(-20, 80).loadGraphic(Paths.image('portraits/confusedElectric', 'shared'));
+		// portraitConfused.setGraphicSize(Std.int(portraitConfused.width * PlayState.daPixelZoom * 0.9));
+		portraitConfused.updateHitbox();
+		portraitConfused.scrollFactor.set();
+		add(portraitConfused);
+		portraitConfused.visible = false;
+
+		portraitAngry = new FlxSprite(-20, 80).loadGraphic(Paths.image('portraits/angryElectric', 'shared'));
+		// portraitAngry.setGraphicSize(Std.int(portraitAngry.width * PlayState.daPixelZoom * 0.9));
+		portraitAngry.updateHitbox();
+		portraitAngry.scrollFactor.set();
+		add(portraitAngry);
+		portraitAngry.visible = false;
+
+		portraitMad = new FlxSprite(-20, 80).loadGraphic(Paths.image('portraits/madElectric', 'shared'));
+		// portraitMad.setGraphicSize(Std.int(portraitMad.width * PlayState.daPixelZoom * 0.9));
+		portraitMad.updateHitbox();
+		portraitMad.scrollFactor.set();
+		add(portraitMad);
+		portraitMad.visible = false;
+
+		portraitBF = new FlxSprite(730, 210).loadGraphic(Paths.image('portraits/bfPortrait', 'shared'));
+		// portraitBF.setGraphicSize(Std.int(portraitBF.width * PlayState.daPixelZoom * 0.9));
+		portraitBF.updateHitbox();
+		portraitBF.scrollFactor.set();
+		add(portraitBF);
+		portraitBF.visible = false;
+		
+		box.animation.play('normalOpen');
+		box.setGraphicSize(Std.int(box.width * PlayState.daPixelZoom * 0.9));
+		box.updateHitbox();
+		add(box);
+
+		box.screenCenter(X);
+
+		if (!talkingRight)
+		{
+			// box.flipX = true;
+		}
+
+		dropText = new FlxText(242, 502, Std.int(FlxG.width * 0.6), "", 32);
+		dropText.font = 'Pixel Arial 11 Bold';
+		dropText.color = 0xFFD89494;
+		add(dropText);
+
+		swagDialogue = new FlxTypeText(240, 500, Std.int(FlxG.width * 0.6), "", 32);
+		swagDialogue.font = 'Pixel Arial 11 Bold';
+		swagDialogue.color = 0xFF3F2021;
+		swagDialogue.sounds = [FlxG.sound.load(Paths.sound('pixelText'), 0.6)];
+		add(swagDialogue);
+
+		dialogue = new Alphabet(0, 80, "", false, true);
+		// dialogue.x = 90;
+		// add(dialogue);
+	}
+
+	var dialogueOpened:Bool = false;
+	var dialogueStarted:Bool = false;
+
+	override function update(elapsed:Float)
+	{
+		dropText.text = swagDialogue.text;
+
+		if (box.animation.curAnim != null)
+		{
+			if (box.animation.curAnim.name == 'normalOpen' && box.animation.curAnim.finished)
+			{
+				box.animation.play('normal');
+				dialogueOpened = true;
+			}
+		}
+
+		if (dialogueOpened && !dialogueStarted)
+		{
+			startDialogue();
+			dialogueStarted = true;
+		}
+
+		if (FlxG.keys.justPressed.ANY  && dialogueStarted == true)
+		{
+			remove(dialogue);
+				
+			FlxG.sound.play(Paths.sound('clickText'), 0.8);
+
+			if (dialogueList[1] == null && dialogueList[0] != null)
+			{
+				if (!isEnding)
+				{
+					isEnding = true;
+
+					new FlxTimer().start(0.2, function(tmr:FlxTimer)
+					{
+						box.alpha -= 1 / 5;
+						bgFade.alpha -= 1 / 5 * 0.7;
+						portraitElectric.visible = false;
+						portraitConfused.visible = false;
+						portraitAngry.visible = false;
+						portraitMad.visible = false;
+						portraitBF.visible = false;
+						swagDialogue.alpha -= 1 / 5;
+						dropText.alpha = swagDialogue.alpha;
+					}, 5);
+
+					new FlxTimer().start(1.2, function(tmr:FlxTimer)
+					{
+						finishThing();
+						kill();
+					});
+				}
+			}
+			else
+			{
+				dialogueList.remove(dialogueList[0]);
+				startDialogue();
+			}
+		}
+		
+		super.update(elapsed);
+	}
+
+	var isEnding:Bool = false;
+
+	function startDialogue():Void
+	{
+		cleanDialog();
+		// var theDialog:Alphabet = new Alphabet(0, 70, dialogueList[0], false, true);
+		// dialogue = theDialog;
+		// add(theDialog);
+
+		// swagDialogue.text = ;
+		swagDialogue.resetText(dialogueList[0]);
+		swagDialogue.start(0.04, true);
+
+		switch (curCharacter)
+		{
+			case 'confused':
+				portraitAngry.visible = false;
+				portraitMad.visible = false;
+				portraitElectric.visible = false;
+				portraitBF.visible = false;
+				box.flipX = true;
+				if (!portraitAngry.visible)
+				{
+					portraitAngry.visible = true;
+					// portraitAngry.animation.play('enter');
+				}
+			case 'emp':
+				portraitAngry.visible = false;
+				portraitMad.visible = false;
+				portraitConfused.visible = false;
+				portraitBF.visible = false;
+				box.flipX = true;
+				if (!portraitMad.visible)
+				{
+					portraitMad.visible = true;
+					// portraitMad.animation.play('enter');
+				}
+			case 'angry':
+				portraitConfused.visible = false;
+				portraitMad.visible = false;
+				portraitElectric.visible = false;
+				portraitBF.visible = false;
+				box.flipX = true;
+				if (!portraitAngry.visible)
+				{
+					portraitAngry.visible = true;
+					// portraitAngry.animation.play('enter');
+				}
+			case 'mad':
+				portraitAngry.visible = false;
+				portraitElectric.visible = false;
+				portraitConfused.visible = false;
+				portraitBF.visible = false;
+				box.flipX = true;
+				if (!portraitMad.visible)
+				{
+					portraitMad.visible = true;
+					// portraitMad.animation.play('enter');
+				}
+			case 'bf':
+				portraitAngry.visible = false;
+				portraitMad.visible = false;
+				portraitConfused.visible = false;
+				portraitElectric.visible = false;
+				box.flipX = false;
+				if (!portraitBF.visible)
+				{
+					portraitBF.visible = true;
+					// portraitBF.animation.play('enter');
+				}
+		}
+	}
+
+	function cleanDialog():Void
+	{
+		var splitName:Array<String> = dialogueList[0].split(":");
+		curCharacter = splitName[1];
+		dialogueList[0] = dialogueList[0].substr(splitName[1].length + 2).trim();
+	}
+}
